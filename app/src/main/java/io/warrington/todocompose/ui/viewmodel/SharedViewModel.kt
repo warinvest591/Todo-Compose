4b2a6f4fe81bc713b2f604de05158ad2d583d6bf
Warrington Bloomfield <warrington.bloomfield@stash.com>
2022-10-18 09:18:42 -0400

    updated search bar functionality

--- app/src/main/java/io/warrington/todocompose/ui/viewmodel/SharedViewModel.kt
+++ app/src/main/java/io/warrington/todocompose/ui/viewmodel/SharedViewModel.kt
@@ -1,10 +1,13 @@
 package io.warrington.todocompose.ui.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.warrington.todocompose.data.models.ToDoTask
 import io.warrington.todocompose.repositories.TodoRepository
+import io.warrington.todocompose.util.SearchAppBarState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
@@ -14,9 +17,12 @@ import javax.inject.Inject
 @HiltViewModel
 class SharedViewModel @Inject constructor(
     private val repository: TodoRepository
-): ViewModel() {
+) : ViewModel() {
 
-    private val allTasksFromDatabase  = MutableStateFlow<List<ToDoTask>>(emptyList())
+    private val allTasksFromDatabase = MutableStateFlow<List<ToDoTask>>(emptyList())
+
+    val searchAppBarState: MutableState<SearchAppBarState> = mutableStateOf(SearchAppBarState.CLOSED)
+    val searchTextState: MutableState<String> = mutableStateOf("")
     val allTasks: StateFlow<List<ToDoTask>> = allTasksFromDatabase
 
     fun getAllTasks() {
