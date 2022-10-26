c02a5dd0554eb826d818ab965f10d6fa0efecc01
Warrington Bloomfield <warrington.bloomfield@stash.com>
2022-10-26 13:31:21 -0400

    added empty content component

---
+++ app/src/main/java/io/warrington/todocompose/ui/screens/list/EmptyContent.kt
@@ -0,0 +1,42 @@
+package io.warrington.todocompose.ui.screens.list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import io.warrington.todocompose.R
+import io.warrington.todocompose.ui.theme.MediumGray
+
+@Composable
+fun EmptyContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = Modifier.size(120.dp),
+            painter = painterResource(id = R.drawable.ic_empty_tasks),
+            contentDescription = "empty icon",
+            tint = MediumGray
+        )
+        Text(
+            text = "No Tasks Found",
+            color = MediumGray,
+            fontWeight = FontWeight.Bold,
+            fontSize = MaterialTheme.typography.h5.fontSize
+        )
+    }
+}
