fbd56aa65e8178632371c218f40b3ad620b03634
Warrington <45803147+warinvest591@users.noreply.github.com>
2022-10-31 07:40:05 -0400

    Add Drop Down (#48)
    * created priority drop down
    
    * fixed race condition
    
    * clean up
    
    Co-authored-by: Warrington Bloomfield <warrington.bloomfield@stash.com>

--- app/src/main/java/io/warrington/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/io/warrington/todocompose/ui/screens/task/TaskScreen.kt
@@ -1,22 +1,64 @@
 package io.warrington.todocompose.ui.screens.task
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import io.warrington.todocompose.data.models.Priority
 import io.warrington.todocompose.data.models.ToDoTask
+import io.warrington.todocompose.ui.viewmodel.SharedViewModel
 import io.warrington.todocompose.util.Action
+import io.warrington.todocompose.util.Action.NO_ACTION
 
 @Composable
 fun TaskScreen(
     selectedTask: ToDoTask?,
+    sharedViewModel: SharedViewModel,
     navigateToListScreen: (Action) -> Unit
 ) {
+
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TaskAppBar(
                 selectedTask = selectedTask,
-                navigateToTaskScreen = navigateToListScreen
+                navigateToTaskScreen = { action ->
+                    when {
+                        action == NO_ACTION -> navigateToListScreen(action)
+                        sharedViewModel.validateFields() -> navigateToListScreen(action)
+                        else -> displayToast(context = context)
+                    }
+                }
             )
         },
-        content = {}
+        content = {
+            selectedTask?.let {
+                TaskContent(
+                    title = title,
+                    onTitleChange = {
+                        sharedViewModel.updateTitle(it)
+                    },
+                    description = description,
+                    onDescriptionChange = {
+                        sharedViewModel.description.value = it
+                    },
+                    priority = priority,
+                    onPrioritySelected = {
+                        sharedViewModel.priority.value = it
+                    }
+                )
+            }
+        }
     )
+}
+
+fun displayToast(context: Context) {
+    Toast.makeText(context, "Fields are empty", Toast.LENGTH_SHORT)
 }
